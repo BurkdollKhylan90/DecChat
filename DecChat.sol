53a78c026add47239887e13b8014bdfbdbca05fd
BurkdollKhylan90 <101615786+BurkdollKhylan90@users.noreply.github.com>
2023-11-21 08:32:05 +0100

    Update DecChat.sol

--- DecChat.sol
+++ DecChat.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DecentralizedChat {
+    mapping(address => string) public nicknames;
+    mapping(address => bool) public registeredAddresses;
+
+    event MessageSent(address indexed sender, string nickname, string message);
+
+
+
+   }
 
