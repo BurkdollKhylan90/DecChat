588d36350166220092f414b01de8bd10d0536178
BurkdollKhylan90 <101615786+BurkdollKhylan90@users.noreply.github.com>
2023-11-21 08:32:20 +0100

    Update DecChat.sol

--- DecChat.sol
+++ DecChat.sol
@@ -10,7 +10,13 @@ modifier onlyRegistered() {
         require(registeredAddresses[msg.sender], "Sender is not registered");
         _;
     }
+function register(string memory _nickname) external {
+        require(bytes(_nickname).length > 0, "Nickname cannot be empty");
+        require(bytes(nicknames[msg.sender]).length == 0, "Sender is already registered");
 
+        nicknames[msg.sender] = _nickname;
+        registeredAddresses[msg.sender] = true;
+    }
 
    }
 
